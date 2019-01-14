2d1710417040c4597caf6640919303298646ab57
Tdog <tianalaurence@gmail.com>
2019-01-13 22:51:44 -0800

    push value into array, add the total value of the array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,64 @@
 
+//empty array to push values randomly genorated 
+var userArray = [];
+var compArray =[];
+var userSum = [];
+
 //the computers random number
 function myFunction() {
     var x = Math.floor((Math.random() * 16) + 4);
     document.getElementById("random").innerHTML = x;
+    userArray.push(random);
+    console.log(compArray.push('random'));
 };
 
 document.getElementById("blue").addEventListener("click", myFunction2);
 //function for the blue image
 function myFunction2() {
-    var x = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("blueP").innerHTML = x;
+    var xBlue = Math.floor((Math.random() * 4) + 1);
+    document.getElementById("blueP").innerHTML = xBlue;
+    userArray.push(xBlue);
+    console.log(userArray.push('xBlue'));
 };
 
 document.getElementById("green").addEventListener("click", myFunction3);
 //function for the green image
 function myFunction3() {
-    var x = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("greenP").innerHTML = x;
+    var xGreen = Math.floor((Math.random() * 4) + 1);
+    document.getElementById("greenP").innerHTML = xGreen;
+    userArray.push(xGreen);
+    console.log(userArray.push('xGreen'));
 };
 
 document.getElementById("red").addEventListener("click", myFunction4);
 //function for the red image
 function myFunction4() {
-    var x = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("redP").innerHTML = x;
+    var xRed = Math.floor((Math.random() * 4) + 1);
+    document.getElementById("redP").innerHTML = xRed;
+    userArray.push(xRed);
+    console.log(userArray.push('xRed'));
 };
 
 document.getElementById("yellow").addEventListener("click", myFunction5);
 
 //function for the yellow image 
 function myFunction5() {
-    var x = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("yellowP").innerHTML = x;
-    console.log()
+    var xYellow = Math.floor((Math.random() * 4) + 1);
+    document.getElementById("yellowP").innerHTML = xYellow;
+    //console.log()
+    userArray.push(xYellow);
+    console.log(userArray.push('xYellow'));
 };
-//click on image and get a random number 
-//$( ":yellow" ).click(function() {
-    //var x = Math.floor((Math.random() * 4) + 1);
-    //document.getElementById("yellowP").innerHTML = x;
-   // console.log( x );
-//});
+
+userSum = [1, 2, 3].reduce(add, 0);
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(userSum);
+
+
 
 // Store alert components in object
 //let alertDiv = {
