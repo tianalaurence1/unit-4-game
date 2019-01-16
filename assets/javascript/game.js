2ebc4e211d496fd8c28416b4e716e342339ba7b5
Tdog <tianalaurence@gmail.com>
2019-01-15 20:02:18 -0800

    Ray fixed 💩)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,78 @@
 
 //empty array to push values randomly genorated 
 var userArray = [];
-var compArray =[];
+var compArray = [];
 var userSum = [];
+var gemValue = [0, 0, 0, 0];
+
+var compNum = 0
+var usrNum = 0
 
 //the computers random number
-function myFunction() {
-    var x = Math.floor((Math.random() * 16) + 4);
-    document.getElementById("random").innerHTML = x;
-    userArray.push(random);
-    console.log(compArray.push('random'));
-};
-
-document.getElementById("blue").addEventListener("click", myFunction2);
-//function for the blue image
-function myFunction2() {
-    var xBlue = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("blueP").innerHTML = xBlue;
-    userArray.push(xBlue);
-    console.log(userArray.push('xBlue'));
-};
-
-document.getElementById("green").addEventListener("click", myFunction3);
-//function for the green image
-function myFunction3() {
-    var xGreen = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("greenP").innerHTML = xGreen;
-    userArray.push(xGreen);
-    console.log(userArray.push('xGreen'));
-};
-
-document.getElementById("red").addEventListener("click", myFunction4);
-//function for the red image
-function myFunction4() {
-    var xRed = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("redP").innerHTML = xRed;
-    userArray.push(xRed);
-    console.log(userArray.push('xRed'));
-};
-
-document.getElementById("yellow").addEventListener("click", myFunction5);
-
-//function for the yellow image 
-function myFunction5() {
-    var xYellow = Math.floor((Math.random() * 4) + 1);
-    document.getElementById("yellowP").innerHTML = xYellow;
-    //console.log()
-    userArray.push(xYellow);
-    console.log(userArray.push('xYellow'));
-};
-
-userSum = [1, 2, 3].reduce(add, 0);
-
-function add(a, b) {
-    return a + b;
+function start() {
+    userSum = []
+    usrNum = 0
+    gemValue = gemValue.map(() => Math.floor((Math.random() * 8) + 1))
+    compNum = Math.floor((Math.random() * 99) + 1)
+    $("#computerGuess").text(compNum)
+    
 }
 
-console.log(userSum);
+function sum(arr) {
+    function add(a, b) {
+        return a + b;
+    }
+    const gemAndTheHologramssum = arr.reduce(add, 0);
+    return gemAndTheHologramssum
+}
+
+function myFunction() {
+    var index = $(this).attr("value")
+
+    userSum.push(gemValue[index])
+    usrNum = sum(userSum)
+   
+
+    if (compNum == usrNum) {
+        alert("You win!!!")
+        start()
+    }
+    if (compNum < usrNum) {
+        alert("You Lose :(")
+        start()
+    }
+ document.getElementById("userGuess").innerHTML = usrNum;
+};
+
+document.getElementById("blue").addEventListener("click", myFunction);
+//function for the blue image
+
+
+document.getElementById("green").addEventListener("click", myFunction);
+//function for the green image
+
+document.getElementById("red").addEventListener("click", myFunction);
+//function for the red image
+
+
+document.getElementById("yellow").addEventListener("click", myFunction);
+
+//function for the yellow image 
+
+
+
+console.log(sum(userSum));
+//console.log(userArray.push('xYellow', 'xBlue', 'xRed'));
 
 
 
 // Store alert components in object
 //let alertDiv = {
-	//loose: '<div class="alert alert-danger" role="alert"><strong>Loser!</strong> HaHaHa you looseee!</div>',
-	//win: '<div class="alert alert-success" role="alert"><strong>Wahoo you win!</strong> Lets play again!</div>',
-	//draw: '<div class="alert alert-info" role="alert"><strong>Close!</strong> it was a draw, please reset and try again.</div>'
+//loose: '<div class="alert alert-danger" role="alert"><strong>Loser!</strong> HaHaHa you looseee!</div>',
+//win: '<div class="alert alert-success" role="alert"><strong>Wahoo you win!</strong> Lets play again!</div>',
+//draw: '<div class="alert alert-info" role="alert"><strong>Close!</strong> it was a draw, please reset and try again.</div>'
 
-
+start()
 // Track player score
 //let yourScore = 0;
 //let myScore = 0;
